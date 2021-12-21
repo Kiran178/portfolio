960aec7ca5ca5bb0d54b90e267796eb0f8e71c01
Kiran kumar <kirankumar17897@gmail.com>
2021-12-21 23:07:36 +0530

    framer-motion

--- src/Animation.js
+++ src/Animation.js
@@ -32,3 +32,30 @@ export const fadeUp = {
     transition: { ease: "easeOut", duration: 1 },
   },
 };
+export const fadeDown = {
+  hidden: { y: -60, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    // transition: { duration: 0.5, ease: "easeOut" },
+  },
+};
+export const fadeRight = {
+  hidden: { x: -500, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+export const bounceRight = {
+  hidden: { x: -500, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 10,
+    },
+  },
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,58 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { fadeDown } from "../Animation";
 
 export default function Header() {
   return (
-    <>
+    <motion.div initial="hidden" animate="visible">
       <Navbar variant="dark" className="header">
         <Container fluid>
-          <Navbar.Brand as={Link} to={"/"}>
-            Kiran
-          </Navbar.Brand>
+          <motion.div
+            variants={fadeDown}
+            transition={{ duration: 0.75, ease: "easeOut" }}
+          >
+            <Navbar.Brand as={Link} to={"/"}>
+              Kiran
+            </Navbar.Brand>
+          </motion.div>
           <Nav className="justify-content-end">
-            <Nav.Link as={Link} to={"/about"}>
-              <span>01. </span>About
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/experience"}>
-              <span>02. </span>Experience
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/work"}>
-              <span>03. </span>Work
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/contact"}>
-              <span>04. </span>Contact
-            </Nav.Link>
+            <motion.div
+              variants={fadeDown}
+              transition={{ delay: 0.25, duration: 0.5, ease: "easeOut" }}
+            >
+              <Nav.Link as={Link} to={"/about"}>
+                <span>01. </span>About
+              </Nav.Link>
+            </motion.div>
+            <motion.div
+              variants={fadeDown}
+              transition={{ delay: 0.35, duration: 0.5, ease: "easeOut" }}
+            >
+              <Nav.Link as={Link} to={"/experience"}>
+                <span>02. </span>Experience
+              </Nav.Link>
+            </motion.div>
+            <motion.div
+              variants={fadeDown}
+              transition={{ delay: 0.45, duration: 0.5, ease: "easeOut" }}
+            >
+              <Nav.Link as={Link} to={"/work"}>
+                <span>03. </span>Work
+              </Nav.Link>
+            </motion.div>
+            <motion.div
+              variants={fadeDown}
+              transition={{ delay: 0.55, duration: 0.5, ease: "easeOut" }}
+            >
+              <Nav.Link as={Link} to={"/contact"}>
+                <span>04. </span>Contact
+              </Nav.Link>
+            </motion.div>
           </Nav>
         </Container>
       </Navbar>
-    </>
+    </motion.div>
   );
 }
